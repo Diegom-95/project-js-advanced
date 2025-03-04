2f3c3f0ba58260218e380a04529eee80a88c46ae
Diegom-95 <diegomaggioni95@gmail.com>
2025-03-04 20:03:55 +0100

    added CSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  devServer: {
-    static: './dist',
-    open: true,
-    hot: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-          { from: './src/img/*', to: 'img/[name][ext]' }// Primo pattern
-      ]
-  })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  }
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devServer: {
+        static: './src', // Modifica qui
+        open: true,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/img/*', to: 'img/[name][ext]' },
+                { from: './src/css/*', to: 'css/[name][ext]' } // Aggiungi questa riga
+            ]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
 };
